5302c1d40015260ebba91a78f045f22b17680d57
Steven Truesdell <steven@strues.io>
2017-04-03 23:17:02 -0600

    fix test

--- src/core/errors/httpError.js
+++ src/core/errors/httpError.js
@@ -8,7 +8,7 @@ class HttpError extends Error {
   constructor(message, status, isPublic) {
     super(message);
     this.name = this.constructor.name;
-    this.message = JSON.stringify(message);
+    this.message = message;
     this.status = status;
     this.isPublic = isPublic;
     this.isOperational = true;
